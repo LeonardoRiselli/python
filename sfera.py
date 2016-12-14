28bca8f2f54ee890d9a533e8d887b56af93e8337
LeonardoRiselli <juniorriselli@gmail.com>
2016-12-14 15:02:55 +0100

    caricato il 14 dic

--- sfera.py
+++ sfera.py
@@ -1,6 +1,6 @@
 import math
 r= input("dammi il valore del raggio: ")
-s= 4*math.pi*r*r
-v= 4/3*math.pi*r*r*r
+s= 4.0*math.pi*r*r
+v= 1.33*math.pi*r*r*r
 print "la superficie e'",s 
 print "il volume e' ", v
