d493218ddab6add931bc52d71826a10b495f3159
LeonardoRiselli <juniorriselli@gmail.com>
2016-12-22 18:40:52 +0100

    caricato il 22 dic

--- primogrado.py
+++ primogrado.py
@@ -1,8 +1,7 @@
 a=input("dimmi il valore di a: ")
 b=input("dimmi il valore di b: ")
+b=b*(-1)
 if b<0:
-  b=b*(-1)
   print"il risutato e': X=",b,"/",a
 else:
-  b=b*(-1)
-  print"il risutato e': X=",b,"/",a
+  print"il risutato e': X=",b,"/",a b=b*(-1)
