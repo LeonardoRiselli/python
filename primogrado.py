c1392bf7ebd958443a061aff30290e547965ac8d
LeonardoRiselli <juniorriselli@gmail.com>
2016-12-22 15:52:35 +0100

    caricato il 22 dic

---
+++ primogrado.py
@@ -0,0 +1,7 @@
+a=input("dimmi il valore di a: ")
+b=input("dimmi il valore di b: ")
+if a<0:
+  x=a-b
+else:
+  x=a+b
+print"il risutato e': X=",x
--- volume.py
+++ volume.py
@@ -1,10 +1,10 @@
 import math
-risp=input("per calcolare il volume di un cubo clicca 1, per calcolare il volume della circonferenza scrivi 2: ")
+risp=input("effettua una schelta 1 - CUBO, 2 - SFERA: ")
 if risp==1:
-  l=input("dimmi il valore del lato")
+  l=input("dimmi il valore del lato: ")
   v= l*l*l
   print"il volume del cubo e': ",v
 else:
   r=input("dimmi il valore del raggio: ")
   v= 4.0/3.0*math.pi*r*r*r
-  print"il valore del volume e': ",v
+  print"il volume e': ",v
