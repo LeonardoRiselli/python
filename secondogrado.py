8dbb0fd022717d7c5b44d58fc6a1722d5fc611f7
LeonardoRiselli <juniorriselli@gmail.com>
2017-01-13 17:53:11 +0100

    caricato il 13/01/2017

--- secondogrado.py
+++ secondogrado.py
@@ -1,6 +1,6 @@
 import math 
-a=input("dimmi il valore di a: ")
-b=input("dimmi il valore di b: ")
+a=float(input("dimmi il valore di a: "))
+b=float(input("dimmi il valore di b: "))
 c=input("dimmi il valore di c: ")
 if a==0 and b==0 and c==0:
   print"0=0"
@@ -13,8 +13,8 @@ else:
   delta=b*b-4.0*a*c
   if delta>0:
     radicedelta= math.sqrt(delta)
-    x1= b*(-1.0) + radicedelta / (2.0*a)
-    x2= b*(-1.0) - radicedelta / (2.0*a)
+    x1= b*(-1.0) + radicedelta / 2.0*a
+    x2= b*(-1.0) - radicedelta / 2.0*a
     print "x1 e uguale a: ",x1,"e x2 e uguale a: ",x2
   else:
     print " e' impossibile "
