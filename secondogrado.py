164c026be40e4e0c8ef09739e5094efb7550c9fb
LeonardoRiselli <juniorriselli@gmail.com>
2017-01-13 15:40:50 +0100

    caricato il 13/01/2017

--- secondogrado.py
+++ secondogrado.py
@@ -6,13 +6,15 @@ delta=b*b-4.0*a*c
 if a==0 and b==0 and c==0:
   print"0=0"
 elif a==0 and b==0:
-    
-  
-
-if delta>0:
-  radicedelta= math.sqrt(delta)
-  x1= (b*(-1.0)) + radicedelta / (2.0*a)
-  x2= (b*(-1.0)) - radicedelta / (2.0*a)
-  print "x1 e uguale a: ",x1,"e x2 e uguale a: ",x2
-else:
-  print " e' impossibile "
+  print"C non può essere uguale a 0"
+elif a==0:
+  x=-c/b
+  print"x=",x
+else:  
+  if delta>0:
+    radicedelta= math.sqrt(delta)
+    x1= (b*(-1.0)) + radicedelta / (2.0*a)
+    x2= (b*(-1.0)) - radicedelta / (2.0*a)
+    print "x1 e uguale a: ",x1,"e x2 e uguale a: ",x2
+  else:
+    print " e' impossibile "
