96f8dbb92396a24a7b99fbec1e50131ca5e88730
LeonardoRiselli <juniorriselli@gmail.com>
2017-01-13 16:43:59 +0100

    caricato il 13/01/2017

--- secondogrado.py
+++ secondogrado.py
@@ -1,20 +1,20 @@
-import math 
+delta=b*b-4.0*a*cimport math 
 a=input("dimmi il valore di a: ")
 b=input("dimmi il valore di b: ")
 c=input("dimmi il valore di c: ")
-delta=b*b-4.0*a*c
 if a==0 and b==0 and c==0:
   print"0=0"
 elif a==0 and b==0:
-  print"C non può essere uguale a 0"
+  print"C non puo' essere uguale a 0"
 elif a==0:
   x=-c/b
   print"x=",x
 else:  
+  delta=b*b-4.0*a*c
   if delta>0:
     radicedelta= math.sqrt(delta)
-    x1= (b*(-1.0)) + radicedelta / (2.0*a)
-    x2= (b*(-1.0)) - radicedelta / (2.0*a)
+    x1= b*(-1.0) + radicedelta / (2.0*a)
+    x2= b*(-1.0) - radicedelta / (2.0*a)
     print "x1 e uguale a: ",x1,"e x2 e uguale a: ",x2
   else:
     print " e' impossibile "
